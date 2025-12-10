7614db855cbcc53d6d83d4c58e49c324425c7a7b
rodrigcasio <rod.casio@outlook.com>
2025-12-09 20:59:00 -0600

    `feat:` Modify syntax for `pylint` high grade

--- server.py
+++ server.py
@@ -1,3 +1,13 @@
+"""
+Executing this server, it deploys the web application using flask.
+It recieves the response from the service of Watson NLP Library 
+with the imported "emotion_detector". 
+This service is used by obtaning the input from the user in the client side, 
+and place this input as an argument for "emotion_detector()". 
+Lastly, we parse the obtained data into a list from 
+the dict returned by the "emotion_detector()" function
+Pylint test grade: 9.52/10.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detection")
@@ -5,28 +15,24 @@ app = Flask("Emotion Detection")
 @app.route('/emotionDetector')
 def sent_to_analyze():
     """
-    Function to obtain the input from the user, using it within the emotion_detectr() function
-    to make use of the emotion detector app with Watson NLP library. Getting this response
-    we extract the specific data and sending it to the client.
+    Function to obtain the input from the user, using it within the emotion_detectr() 
+    function to make use of the emotion detector app with Watson NLP library. 
+    Getting this response, we extract the specific data and sending it to the client.
     """
-    text_to_analyze = request.args.get('textToAnalyze') # 3 hours figuring out it is "textToAnalyze" NOT "TextToAnalyze"
-    
+    text_to_analyze = request.args.get('textToAnalyze')
     if not text_to_analyze or text_to_analyze.strip() == '':
         return "Invalid text! Please try again!."
-    
     response = emotion_detector(text_to_analyze)
     dominant_emotion = response.pop("dominant_emotion")
-
     if dominant_emotion is None:
         return "Invalid text! Please try again!."
-    
     formatted_scores = []
     for key, value in response.items():
         formatted_scores.append(f"'{key}': {value}")
     # or formatted_scores = [f"'{emotion}': {score}" for emotion, scores in response.items()]
-    
     scores_string = ', '.join(formatted_scores)
-    return f"For the given statement, the system response is {scores_string}. The dominant emotion is {dominant_emotion}"
+    final_string = f"For the given statement, the system response is {scores_string}. The dominant emotion is {dominant_emotion}"
+    return final_string
 
 @app.route('/')
 def render_index_page():
